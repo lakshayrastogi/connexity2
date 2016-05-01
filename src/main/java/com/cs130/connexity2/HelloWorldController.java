806a1cbd68f5c4c06099cb621d64d8dba3946092
kalinkhemka <kalinkhemka@gmail.com>
2016-04-30 19:53:19 -0700

    Added HTML File

--- src/main/java/com/cs130/connexity2/HelloWorldController.java
+++ src/main/java/com/cs130/connexity2/HelloWorldController.java
@@ -1,7 +1,9 @@
 package com.cs130.connexity2;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloWorldController {
@@ -9,6 +11,12 @@ public class HelloWorldController {
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+    
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+    	return "hello.html";
     }
 	
 }
