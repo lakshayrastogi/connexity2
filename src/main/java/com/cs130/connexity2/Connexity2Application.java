fe51625d8f7aa7106ae12cac884e27ff96ef8a4d
classykeyser <dekeyser45@gmail.com>
2016-05-03 02:47:19 -0700

    Added main.html

--- src/main/java/com/cs130/connexity2/Connexity2Application.java
+++ src/main/java/com/cs130/connexity2/Connexity2Application.java
@@ -10,15 +10,16 @@ public class Connexity2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Connexity2Application.class, args);
-//		ApplicationContext ctx = SpringApplication.run(Connexity2Application.class, args);
 		
-//		System.out.println("Let's inspect the beans provided by Spring Boot:");
-//
-//        String[] beanNames = ctx.getBeanDefinitionNames();
-//        Arrays.sort(beanNames);
-//        for (String beanName : beanNames) {
-//            System.out.println(beanName);
-//        }
+		/*ApplicationContext ctx = SpringApplication.run(Connexity2Application.class, args);
 		
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) 
+			System.out.println(beanName);
+		}
+	}*/
 	}
 }
