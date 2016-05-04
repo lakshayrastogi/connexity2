ff27f322be3eb88aab7890ace409459301496935
mnadir14 <mnadir14@gmail.com>
2016-05-03 18:06:50 -0700

    Moved SearchResult json parser from SearchResult constructor to private function convertToSearchResult in CatalogSearchClient.java

--- src/main/java/com/cs130/connexity2/CatalogSearchClient.java
+++ src/main/java/com/cs130/connexity2/CatalogSearchClient.java
@@ -34,6 +34,74 @@ public class CatalogSearchClient {
 	            reader.close();
 	    }
 	}
+	
+	private SearchResult convertToSearchResult(JSONObject jsonRes) throws NullPointerException {
+		SearchResult  res = new SearchResult();
+		try {
+			res.setMerchantId(((Long) jsonRes.get("merchantId")).intValue());
+		} catch (Exception e) {res.setMerchantId(-1); e.printStackTrace();}
+		try {
+			res.setCategoryId(((Long) jsonRes.get("categoryId")).intValue());
+		} catch (Exception e) {res.setCategoryId(-1); e.printStackTrace();}
+		try {
+			res.setId(((Long) jsonRes.get("id")).intValue());
+		} catch (Exception e) {res.setId(-1); e.printStackTrace();}
+		try {
+			res.setTitle((String) jsonRes.get("title"));
+		} catch (Exception e) {res.setTitle(null); e.printStackTrace();}
+		try {
+			res.setBrandName((String) ((JSONObject) jsonRes.get("brand")).get("name"));
+		} catch (Exception e) {res.setBrandName(null); e.printStackTrace();}
+		try {
+			res.setDescription((String) jsonRes.get("description"));
+		} catch (Exception e) {res.setDescription(null); e.printStackTrace();}
+		try {
+			res.setUrl((String) ((JSONObject) jsonRes.get("url")).get("value"));
+		} catch (Exception e) {res.setUrl(null); e.printStackTrace();}
+		try {
+			List<String> images = new ArrayList<>();
+			JSONArray jsonArr = (JSONArray) ((JSONObject) jsonRes.get("images")).get("image");
+			for (int i = 0; i < jsonArr.size(); i++) {
+				String imageUrl = (String) ((JSONObject) jsonArr.get(i)).get("value");
+				images.add(imageUrl);
+			}
+			res.setImages(images);
+		} catch (Exception e) {res.setImages(null); e.printStackTrace();}
+		try {
+			res.setSku((String) jsonRes.get("sku"));
+		} catch (Exception e) {res.setSku(null); e.printStackTrace();}
+		try {
+			res.setDetailUrl((String) ((JSONObject) jsonRes.get("detailUrl")).get("value"));
+		} catch (Exception e) {res.setDetailUrl(null); e.printStackTrace();}
+		try {
+			res.setPrice((((Long)((JSONObject) jsonRes.get("price")).get("integral")).doubleValue()/100.0));
+		} catch (Exception e) {res.setPrice(-1); e.printStackTrace();}
+		try {
+			res.setOriginalPrice((((Long)((JSONObject) jsonRes.get("originalPrice")).get("integral")).doubleValue())/100.0);
+		} catch (Exception e) {res.setOriginalPrice(-1); e.printStackTrace();}
+		try {
+			res.setMarkdownPercent((Double) jsonRes.get("markdownPercent"));
+		} catch (Exception e) {res.setMarkdownPercent(-1); e.printStackTrace();}
+		try {
+			res.setBidded ((boolean) jsonRes.get("bidded"));
+		} catch (Exception e) {res.setBidded(false); e.printStackTrace();}
+		try {
+			res.setMerchantName((String) jsonRes.get("merchantName"));
+		} catch (Exception e) {res.setMerchantName(null); e.printStackTrace();}
+		try {
+			res.setMerchantCertified((boolean) (((JSONObject) jsonRes.get("merchantCertification")).get("certified")));
+		} catch (Exception e) {res.setMerchantCertified(false); e.printStackTrace();}
+		try {
+			res.setMerchantLogoUrl((String) jsonRes.get("merchantLogoUrl"));
+		} catch (Exception e) {res.setMerchantLogoUrl(null); e.printStackTrace();}
+		try {
+			res.setCondition((String) jsonRes.get("condition"));
+		} catch (Exception e) {res.setCondition(null); e.printStackTrace();}
+		try {
+			res.setRelevancy((Double) jsonRes.get("relevancy"));
+		} catch (Exception e) {res.setRelevancy(-1); e.printStackTrace();}
+		return res;
+	}
 
 	public List<SearchResult> getSearchResults(String urlString) {
 		try {
@@ -46,7 +114,7 @@ public class CatalogSearchClient {
 				JSONObject jsonRes = (JSONObject) jsonArr.get(i);
 				/*SearchResult constructor parses JSONObject and 
 				 * sets member variables accordingly */
-				searchResults.add(new SearchResult(jsonRes));
+				searchResults.add(convertToSearchResult(jsonRes));
 			}
 			return searchResults;
 		} catch (ParseException e) {
